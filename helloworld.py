9c8256265e053cdd35de65b311d4070e8fd3501a
shyrazkhan <shyrazkhan@hotmail.com>
2018-11-03 17:53:48 +0500

    python testing file

---
+++ helloworld.py
@@ -0,0 +1,16 @@
+shopList = ["Apple", "Bananas", "Oranges", "Grapes"]
+del shopList[3]
+print(shopList)
+
+if (11 % 2 == 0):
+    print("Even")
+else:
+    print("Odd")
+
+
+for item in shopList:
+    print(item)
+
+for num in range(0,10):
+    print(num)
+
